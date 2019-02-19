f0c4246898192cd661b4b579178d9b9e3bc7af2e
Александр Ящук <a.yashuk@pflb.ru>
2019-02-19 12:04:59 +0300

    Отображение шагов тестов в Аллюре отчёте.

--- src/test/java/ru/performancelab/tests/suites/Smoke/Smoke.java
+++ src/test/java/ru/performancelab/tests/suites/Smoke/Smoke.java
@@ -4,6 +4,8 @@ package ru.performancelab.tests.suites.Smoke;
 import cucumber.api.java.ru.И;
 import cucumber.api.java.ru.Пусть;
 import cucumber.api.java.ru.Тогда;
+import io.qameta.allure.Description;
+import io.qameta.allure.Step;
 import org.openqa.selenium.By;
 import com.codeborne.selenide.Condition;
 import static com.codeborne.selenide.Selenide.*;
@@ -13,8 +15,10 @@ import ru.performancelab.tests.BaseTest;
 
 public class Smoke extends BaseTest {
 
+    @Step("Шаг - Метод 'openHost'")
+    @Description("Описание метода 'openHost'")
     @Пусть("открыт сайт")
-    public void openHos() {
+    public void openHost() {
         open(System.getProperty("testUrl"));
         log.info("Сайт удачно открыт");
 
@@ -23,12 +27,16 @@ public class Smoke extends BaseTest {
         screenshot(screenshotFileName);
     }
 
+    @Step("Шаг - Метод 'mustBeElementWithTheTagH'")
+    @Description("Описание метода 'mustBeElementWithTheTagH'")
     @Тогда("должен быть элемент с тегом H{int}")
     public void mustBeElementWithTheTagH(int arg0) {
         $(By.xpath(String.format("//H%s", arg0))).shouldBe(Condition.visible);
         log.info(String.format("Елемент с тегом 'H%s' обнаружен", arg0));
     }
 
+    @Step("Шаг - Метод 'textTagHElementMustBeSpecifiedString'")
+    @Description("Описание метода 'textTagHElementMustBeSpecifiedString'")
     @И("текст элемента с тегом H{int} должен содержать {string}")
     public void textTagHElementMustBeSpecifiedString(int arg0, String arg1) {
         $(By.xpath(String.format("//H%s[contains(string(),'%s')]", arg0, arg1))).shouldBe(Condition.visible);
